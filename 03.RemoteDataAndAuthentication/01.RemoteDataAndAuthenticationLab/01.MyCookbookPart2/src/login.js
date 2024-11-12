d4ddc1d69e50ee18026757d038c156a0b08d9f21
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-11-12 21:08:57 +0200

    add 01.MyCookbookPart2

---
+++ 03.RemoteDataAndAuthentication/01.RemoteDataAndAuthenticationLab/01.MyCookbookPart2/src/login.js
@@ -0,0 +1,34 @@
+const loginUrl = 'http://localhost:3030/users/login';
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (ev => {
+    ev.preventDefault();
+    const formData = new FormData(ev.target);
+    onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
+}));
+
+async function onSubmit(data) {
+    const body = JSON.stringify({
+        email: data.email,
+        password: data.password,
+    });
+
+    try {
+        const response = await fetch(loginUrl, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body
+        });
+        const data = await response.json();
+        if (response.status == 200) {
+            sessionStorage.setItem('authToken', data.accessToken);
+            window.location.pathname = 'index.html';
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+}
